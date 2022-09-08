bf04763a4da9a33b24fb9e08d6d87c6095b52e6c
stevewanash <stevewanash1@gmail.com>
2022-09-08 06:58:22 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+/**
+ * main - prints a string
+ * no parameters
+ *
+ * Return: 0 ends the function
+ */
+
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
