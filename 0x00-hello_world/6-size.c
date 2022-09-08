0e8e64a0a14d74ee08e899bcc20606a18f1797c7
stevewanash <stevewanash1@gmail.com>
2022-09-08 07:31:43 -0700

    type2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,19 @@
-#include <stdio.h>
+# include <stdio.h>
 
 /**
- * main - give size of various data types
- * @data_type: name of data type
+ * main - prints various data types sizes
+ * @data_type: name of the data type
  *
- * Return: 0 denotes invalid data type
- * or end of function
+ * Return: 0 denotes an invalid data type
+ * or the end of the function
  */
 
 int main(char data_type)
 {
 
-	int size_result = sizeof(data_type);
+	size_result = sizeof(data_type);
 
-	printf("size of a %c: %d byte(s)\n, data_type, size_result);
+	printf("Size of a %c: %d byte(s)\n", data_type, size_result);
 
 	return (0);
 
