cffb017c2b196ee5ea23664822bd50e5dfbf2cd5
stevewanash <stevewanash1@gmail.com>
2022-09-08 06:53:11 -0700

    puts6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 
-	char string1[] = “\"Programming is like building a multilingual puzzle”;
+	char string1[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(string1);
 
