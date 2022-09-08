33c7a450f5660283cf4f3dcfc96870035d2e5d6f
stevewanash <stevewanash1@gmail.com>
2022-09-08 05:58:51 -0700

    puts2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,16 @@
-#!/bin/bash
+# !/bin/bash
+
 /**
-* function prints out a string
+* function main prints out a string
 * also returns an integer value
 */
-int main()
+
+int main(void)
 {
 	char string1[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(string1);
+
 	return 0;
+
 }
