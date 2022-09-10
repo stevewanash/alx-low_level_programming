194279cce92dd1e69c2812bac3485a63b9b4c30c
stevewanash <stevewanash1@gmail.com>
2022-09-09 19:25:06 -0700

    except

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - prints out all the alphabet
+ * letters in small case
+ * no parameters
+ *
+ *Return: 0 denotes the end of the function
+ */
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == q || c == e)
+			continue;
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
