07ff7214c3b2dcb88206abaad8e593d2e1f5d1f6
stevewanash <stevewanash1@gmail.com>
2022-09-09 19:43:03 -0700

    teba

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - prints out all the alphabet
+ * letters in small case
+ * no parameters
+ *
+ *Return: 0 denotes the end of the function
+ */
+int main(void)
+{
+	char c;
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
