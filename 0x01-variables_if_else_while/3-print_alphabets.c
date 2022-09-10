8f8e984a3485d77a3b3bef93dee6cb7194eeeefd
stevewanash <stevewanash1@gmail.com>
2022-09-09 19:20:13 -0700

    uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - prints out all the alphabet
+ * letters in small case
+ * no parameters
+ *
+ *Return: 0 denotes the end of the function
+ */
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
