b6632564bb6daccbf3abc443b0c4a78200beab26
stevewanash <stevewanash1@gmail.com>
2022-09-09 18:54:08 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - prints out all the alphabet
+ * letters in small case
+ * no parameters
+ *
+ *Return: 0 denotes the end of the function
+ */
+int main(void)
+{
+	char c;
+	for (c = 'A'; c <= 'Z'; ++c)
+	{
+		putchar(c);
+	}
+	return (0);
+}
