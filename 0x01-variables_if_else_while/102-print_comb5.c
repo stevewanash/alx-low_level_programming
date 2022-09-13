4fb7ad458a15bf7ee8cefec85a39a5e0e09c7a7a
stevewanash <stevewanash1@gmail.com>
2022-09-12 19:10:03 -0700

    combination of two numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+# include <stdio.h>
+
+/**
+ * main - prints all possible combinations
+ * of one digit numbers
+ * no parameters
+ *
+ * Return: 0 denotes the end of the
+ * function
+ */
+int main(void)
+{
+	int j;
+	int k;
+	int l;
+	int m;
+	for (j = 0; j < 10; j++)
+	{
+		for (k = 0; k < 10; k++)
+		{
+			for (l = 0; l < 10; l++)
+			{
+				for (m = 0; m < 10; m++)
+				{
+					if (k + j == l + m || l + m < k + j)
+					{
+						continue;
+					}
+					putchar(j + '0');
+					putchar(k + '0');
+					putchar(' ');
+					putchar(l + '0');
+					putchar(m + '0');
+					if (k + 1 == j && j == 9)
+					{
+						putchar('\n');
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
+}
