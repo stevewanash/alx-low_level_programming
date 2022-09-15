84974a15da35626d017dbde136a8207931da489b
stevewanash <stevewanash1@gmail.com>
2022-09-15 15:09:03 -0700

    sign2

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,14 +15,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
 		_putchar('+');
+		return (1);
 	}
 	if (n == 0)
 	{
-		return(0);
 		_putchar('0');
+		return(0);
 	}
-	return (-1);
 	_putchar('-');
+	return (-1);
 }
