89ed388caab3aa37d748e759c6b51238744e591f
stevewanash <stevewanash1@gmail.com>
2022-09-14 17:34:28 -0700

    alphabet4

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
  * print_alphabet - prints the alphabet
  * in lower case
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,24 +1,6 @@
 #include <stdio.h>
 
 /**
- * put_char - prints the function _putchar
- * no arguments
- *
- * Return: 0 always
- */
-int put_char(void)
-{
-	int n;
-	char result[] = "_putchar";
-	for (n = 0; n < 8; n++)
-	{
-		putchar(result[n]);
-	}
-	putchar('\n');
-	return (0);
-}
-
-/**
  * print_alphabet - prints the alphabet
  * in lower case
  * no arguments
