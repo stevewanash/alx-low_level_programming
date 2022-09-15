0e1bcc07c0ca9d98f31f49392dc503ba9dda90ff
stevewanash <stevewanash1@gmail.com>
2022-09-14 21:34:27 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,16 @@
-# include <stdio.h>
-# include <ctype.h>
-
-/**
- * print_alphabet_x10 - checks for lowercase
- * characters
- * @c: is the input to be checked for case
- *
- *Return: 1 if input is lowercase
- * 0 otherwise
- */
-
-int _islower(int c)
-{
-	return (islower(c));
+# include <stdio.h>                                                                                                         
+# include <ctype.h>                                                                                                         
+                                                                                                                            
+/**                                                                                                                         
+ * _islower - checks for lowercase                                                                                
+ * characters                                                                                                               
+ * @c: is the input to be checked for case                                                                                  
+ *                                                                                                                          
+ *Return: 1 if input is lowercase                                                                                           
+ * 0 otherwise                                                                                                              
+ */                                                                                                                         
+                                                                                                                            
+int _islower(int c)                                                                                                         
+{                                                                                                                           
+        return (islower(c + '0'));                                                                                                
 }
