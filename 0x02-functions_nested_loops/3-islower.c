7f6e5e04b3166f9dfaf61b3304d11a8c6bb25741
stevewanash <stevewanash1@gmail.com>
2022-09-14 18:53:29 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+/**
+ * print_alphabet_x10 - checks for lowercase
+ * characters
+ * @c: is the input to be checked for case
+ *
+ *Return: 1 if input is lowercase
+ * 0 otherwise
+ */
+
+int _islower(int c)
+{
+	return (islower(c));
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 void print_alphabet(void);
 int put_char(void);
 void print_alphabet_x10(void);
+int _islower(int c);
