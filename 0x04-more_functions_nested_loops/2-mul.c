544c2f123b7b4df151fed56a18fdb0cbe61d55a4
stevewanash <stevewanash1@gmail.com>
2022-09-15 16:00:44 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+# include <stdio.h>                                                                                                         
+                                                                                                                            
+                                                                                                                            
+/**                                                                                                                         
+ * mul - multiplies two numbers                                                                                                                                                                                                  
+ * @c: first number being multiplied                                                                                 
+ * @b: second number being multiplied 
+ *
+ * Return: result of multiplication                                                                                       
+ * of the two numbers                                                                                                         
+ */                                                                                                                         
+                                                                                                                            
+int mul(int a, int b)                                                                                                         
+{                                                                                                                                                                                                                        
+        return (a * b);                                                                                                         
+}
