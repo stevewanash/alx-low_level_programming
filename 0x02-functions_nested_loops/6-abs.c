28bd4186e5e96567691b970728a6c39e19583c40
stevewanash <stevewanash1@gmail.com>
2022-09-14 19:15:00 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+/**
+ * _abs - checks for absolute value
+ * of input integer
+ * @a: is the input to be checked for absolute value
+ *
+ *Return: absolute value of input integer
+ * 
+ */
+
+int _abs(int a)
+{
+	return (abs(a));
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,3 +2,4 @@ void print_alphabet(void);
 int put_char(void);
 void print_alphabet_x10(void);
 int _islower(int c);
+int _abs(int a);
