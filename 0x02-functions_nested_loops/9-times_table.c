81f6842801ab7a10eec1771bc7f0ed30c2f995a2
stevewanash <stevewanash1@gmail.com>
2022-09-15 12:38:26 -0700

    multiplier

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,28 +1,32 @@
-# include <stdio.h>
-
-/**
- * times_table - mutliplication table
- * 
- * no parameters
- *
- *
- * 
- */
-
-void times_table(void)
-{
-	int i;
-	int j;
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{	
-			if (j == 9)
-			{
-				printf("%d\n", i * j);
-				continue;
-			}
-			printf("%d,  ", i * j);
-		}
-	}
+# include <stdio.h>                                                                                                         
+                                                                                                                            
+/**                                                                                                                         
+ * times_table - mutliplication table                                                                                       
+ *                                                                                                                          
+ * no parameters                                                                                                            
+ *                                                                                                                          
+ *                                                                                                                          
+ *                                                                                                                          
+ */ 
+void times_table(void)                                          
+{                                                              
+        int i;                                                  
+        int j;                                                  
+        for (i = 0; i < 10; i++)                                
+        {                                                        
+                for (j = 0; j < 10; j++)                         
+                {                                            
+                        if (j == 9)                                 
+                        {                                          
+                                printf("%d\n", i * j);              
+                                continue;                         
+                        }           
+                        if (i * (j + 1) > 9)
+                        {
+                            printf("%d, ", i * j);
+                            continue;
+                        }
+                        printf("%d,  ", i * j);                  
+                }                                                   
+        }                                                       
 }
