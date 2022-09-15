c82cfe477d3c1e9d840ffa17f23c5ef53eb3a73a
stevewanash <stevewanash1@gmail.com>
2022-09-14 17:58:17 -0700

    alphabet5

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
+
+/**                                                                                                                         
+ * put_char - prints the function _putchar                                                                                  
+ * no arguments                                                                                                             
+ *                                                                                                                          
+ * Return: 0 always                                                                                                         
+ */                                                                                                                         
+int put_char(void)                                                                                                          
+{                                                                                                                           
+        int n;                                                                                                              
+        char result[] = "_putchar";                                                                                         
+        for (n = 0; n < 8; n++)                                                                                             
+        {                                                                                                                   
+                putchar(result[n]);                                                                                         
+        }                                                                                                                   
+        putchar('\n');                                                                                                      
+        return (0);                                                                                                         
+}
 
 /**
  * print_alphabet - prints the alphabet
@@ -15,3 +36,5 @@ void print_alphabet(void)
 	}
 	putchar('\n');
 }
+
+#endif
