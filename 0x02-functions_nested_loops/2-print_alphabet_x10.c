ed48b83ca17e53f508433979f0e7043bb6826490
stevewanash <stevewanash1@gmail.com>
2022-09-14 18:33:22 -0700

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,31 @@
+include <stdio.h>
+
+/**
+ * print_alphabet - prints the alphabet
+ * in lower case 10 times
+ * no arguments
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	
+	char alphabet;
+	
+	int i;
+	
+	for(i = 0; i < 10; i++)
+	{
+		
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			
+			putchar(alphabet);
+			
+		}
+		
+		putchar('\n');
+		
+	}
+	
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 void print_alphabet(void);
 int put_char(void);
+void print_alphabet_x10(void);
