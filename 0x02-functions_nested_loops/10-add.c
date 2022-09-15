0081a48a19f5066a9f7bfa979281dfff9a82d02d
stevewanash <stevewanash1@gmail.com>
2022-09-14 20:25:44 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**                                                                                                                         
+ * put_char - adds two integers                                                                                 
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum of the two integers                                                                                                         
+ */                                                                                                                         
+int add(int a, int b)                                                                                                        
+{                                                                                                                           
+         return (a + b);                                                                                                    
+}
