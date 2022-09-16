f396939ee6a40ba78f8d74ce58cdce5ec1490578
stevewanash <stevewanash1@gmail.com>
2022-09-15 18:20:43 -0700

    diagonal8

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,27 +1,23 @@
 # include <stdio.h>                                                                                                         
-# include "main.h"                                                                                                          
-                                                                                                                            
-/**                                                                                                                         
- * print_diagonal - prints square using "#"                                                                                 
-                                                                                                                            
- *                                                                                                                          
- * @n: size of square "#" should print                                                                                      
- *                                                                                                                          
- * Return: none                                                                                                             
- */                                                                                                                         
-                                                                                                                            
-void print_diagonal(int n)                                                                                                  
-                                                                                                                            
+# include "main.h"
+
+/**
+ * print_diagonal - prints diagonal using "_"
+ * @n: size of square "#" should print
+ *
+ * Return: none
+ */
+void print_diagonal(int n)
 {                                                                                                                           
-        int c;                                                                                                              
-        int d; 
+        int c;
+        int d;
 	if (n <= 0)
 		_putchar('\n');
-        for (c = 1; c <= n; c++)                                                                                            
-        {                                                                                                                   
-                _putchar('\\');                                                                                             
-                _putchar('\n');                                                                                             
-                for (d = 0; (d < c && c < n) ; d++)                                                                                     
-                        _putchar(' ');                                                                                      
-        }                                                                                                                   
+        for (c = 1; c <= n; c++)
+        {
+                _putchar('\\');
+                _putchar('\n');
+                for (d = 0; (d < c && c < n) ; d++)
+                        _putchar(' ');
+        }
 }
