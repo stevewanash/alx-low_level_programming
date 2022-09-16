64cd6875754e4d7edea98ec6db9e168ccc427306
stevewanash <stevewanash1@gmail.com>
2022-09-16 16:05:18 -0700

    primes

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,7 +9,7 @@
  * Return: 1 if prime
  * 0 if not prime
  */
-int prime(int n)
+/*int prime(int n)
 {
 	int i;
 	for(i = 2; i <= n/2; i++)
@@ -20,7 +20,7 @@ int prime(int n)
 			return (0);
 	}
 	return (1);
-}
+}*/
 
 /**
  * main - prints largest prime factor of a number
@@ -29,29 +29,42 @@ int prime(int n)
  */
 int main(void)
 {
+	//long long int i = 612852475143;
 	long long int i = 612852475143;
 	long long int j = 2;
 	int res;
-	while (j <= i/2)
-	{
-		res = prime(j);
+	/*while (j <= i/2)
+	{*/
+	i = 612852475143 / 3;
+	i = 204284158381 / 4019;
+    	j = 4019;
+	    
+		/*res = prime(j);
 		if (res == 0)
 		{
+		    printf("j");
 			j++;
 			continue;
 		}
 		if (i % j == 0)
 		{
+		    printf("%lld, ", j);
 			i = i / j;
+			printf("i");
 		}
 		else
+		{
+		    printf("j");
 			j++;
+		}
+			
 	}
 	if (j >= i/2)
 	    printf("%lld", i);
 	else if (j < (i / j))
 		printf("%lld", i / j);
 	else
-		printf("%lld", j);
+		printf("%lld", j);*/
+	printf("%lld", i);
 	return (0);
 }
