daac6830952bc9deda45a51e5696376a788a10b8
stevewanash <stevewanash1@gmail.com>
2022-09-15 20:03:53 -0700

    mm

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,14 +14,13 @@ void print_times_table(int n)
         int j;                                                                                                              
         for (i = 0; i < n; i++)                                                                                            
         {
-
+		if (n > 15 || n < 0)
+		{
+			printf("\n");
+			break;
+		}
                 for (j = 0; j < n; j++)                                                                                    
                 { 
-			if (n > 15 || n < 0)
-			{
-				printf("\n");
-				break;
-			}
                         if (j == n)                                                                                         
                         {
                                 printf("%d\n", i * j);                                                                      
