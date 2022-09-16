93102d28e573c9702167b8e7408dca5b95b10b25
stevewanash <stevewanash1@gmail.com>
2022-09-16 09:38:38 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers 1- 100 except:
+ * multiples of 3 replaced by "fizz"
+ * multiples of 5 replaced by "buzz"
+ * multiples of both replaced by "fizz buzz"
+ *
+ * Return: 0 always.
+ */
+int main(void)
+{
+	int i;
+	for (i = 1; i <= 100; i++)
+	{
+		if (!(i == 100))
+		{
+			if (!((i % 3) == 0) && !((i % 5) == 0))
+				printf("%d ", i);
+			if ((i % 3) == 0)
+				printf("Fizz ");
+			if ((i % 5) == 0)
+				printf("Buzz ");
+			if ((i % 3) == 0 && (i % 5) == 0)
+				printf("FizzBuzz ");
+			continue;
+		}
+		printf("Buzz\n");
+	}
+	return (0);
+}
