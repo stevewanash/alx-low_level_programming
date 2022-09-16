a70225d9d2d34a587ecc1defde52339f4a9eba31
stevewanash <stevewanash1@gmail.com>
2022-09-16 16:42:18 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - prints numbers
+ * @n: number to be printed
+ *
+ * Return: none
+ */
+
+void print_number(int n)
+{
+	
+	_putchar(n + '0');
+	
+}
