c5964d39b9585e339c1f5e048ec9e1598fc9f453
stevewanash <stevewanash1@gmail.com>
2022-09-16 17:25:41 -0700

    numbers2

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,8 +9,41 @@
  */
 
 void print_number(int n)
-{
+{      
+	int r;
+	if (n < 0)
+	{
+        	putchar('-');
+        	n = -n;
+    	}
+		
+	else if (n > 9999)
+	{
+		_putchar((n/10000)+ '0');
+		r = n%10000;
+		_putchar((r/1000)+ '0');
+		r = r%1000;
+		_putchar((r/100)+ '0');
+		r = r%100;
+		_putchar((r/10)+ '0');
+	}
+	else if (n > 999)   
+	{
+		_putchar((n/1000)+ '0');
+		r = n%1000;
+		_putchar((r/100)+ '0');
+		r = r%100;
+		_putchar((r/10)+ '0');
+	}
+	else if (n > 99)     
+	{
+		_putchar((n/100)+ '0');
+		r = n%100;
+		_putchar((r/10)+ '0');
+	}
+	else if (n > 9)                                                                                           
+		_putchar((n/10)+ '0'); 
 	
-	_putchar(n + '0');
+	_putchar((n%10)+ '0'); 
 	
 }
