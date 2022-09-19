fe2ec3ccfb4f455d12a4db2598d727672d816a1e
stevewanash <stevewanash1@gmail.com>
2022-09-19 16:08:19 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * _puts - prints a string
+ *
+ * @str: string to be printed
+ *
+ * No return
+ */
+
+void _puts(char *str);
+{
+
+	int a = 0;
+
+	while (str[a] != '\0')
+		putchar(str[a]);
+
+		a++;
+
+	putchar('\n');
+
+}
