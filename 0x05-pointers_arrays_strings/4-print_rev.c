7771408060eb4b316535bcdd34e898acd7ef5692
stevewanash <stevewanash1@gmail.com>
2022-09-19 16:34:35 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: string to be printed
+ *
+ * No return
+ */
+
+void print_rev(char *s)
+{
+
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+
+		a++;
+
+	}
+
+	a -= 1;
+
+	while (s[a] >= 0)
+	{
+
+		putchar(str[a]);
+
+		a--;
+
+	}
+
+	putchar('\n');
+
+}
