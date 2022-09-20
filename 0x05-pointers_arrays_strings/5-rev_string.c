8c1eb5eac5237a261eee46fcec6c24f017c7ca1e
stevewanash <stevewanash1@gmail.com>
2022-09-19 17:46:46 -0700

    revvv

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,48 +10,32 @@
 
 void rev_string(char *s)
 {
-
 	int a = 0;
-
 	int b = 0;
-
 	int c = 0;
-
-	char d[100];
-
-	char e[100];
-
+	char d[500];
+	char e[500];
 	while (s[c] != '\0')
 	{
-
 		d[c] = s[c];
-
 		e[c] = s[c];
-
 		c++;
-
 	}
-
+	
 	c -= 1;
-
-	while (c >= 0)
+	
+	while (c >= 0)                                                                                              
 	{
-
 		if (c == b)
 			while (s[a] != '\0')
 			{
 				d[a] = e[a];
 				a++;
-			}
-
+	        	}
 		s[c] = d[b];
-
 		c--;
-
 		b++;
 
 	}
-
-	putchar('\n');
 
 }
