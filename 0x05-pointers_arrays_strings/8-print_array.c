fd41669d8c4fbc2f822e5bf3e0826f703c5d11fc
stevewanash <stevewanash1@gmail.com>
2022-09-19 19:27:24 -0700

    firstn

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * print_array - prints specific number of elements
+ * from array
+ *
+ * @a: array from which elements are printed
+ * @n: number of elements to be printed
+ *
+ * No return
+ */
+
+void print_array(int *a, int n)
+{
+
+	int b = 0;
+
+	while (b < n)
+	{
+
+		if (b + 1 == n)
+		{
+
+			printf("%d\n", a[b]);
+
+			break;
+
+		}
+
+		printf("%d, ", a[b]);
+
+		b++;
+
+	}
+
+}
