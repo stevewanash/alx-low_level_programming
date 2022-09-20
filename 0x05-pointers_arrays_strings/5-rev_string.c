841539f9296962ff40bdaaa01ebe9fbc5d0767db
stevewanash <stevewanash1@gmail.com>
2022-09-19 17:37:13 -0700

    revstr

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/**
+ * rev_string - prints a string in reverse
+ *
+ * @s: string to be printed
+ *
+ * No return
+ */
+
+void rev_string(char *s)
+{
+
+	int a = 0;
+
+	int b = 0;
+
+	int c = 0;
+
+	char d[100];
+
+	char e[100];
+
+	while (s[c] != '\0')
+	{
+
+		d[c] = s[c];
+
+		e[c] = s[c];
+
+		c++;
+
+	}
+
+	c -= 1;
+
+	while (c >= 0)
+	{
+
+		if (c == b)
+			while (s[a] != '\0')
+			{
+				d[a] = e[a];
+				a++;
+			}
+
+		s[c] = d[b];
+
+		c--;
+
+		b++;
+
+	}
+
+	putchar('\n');
+
+}
