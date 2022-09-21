a324fe35f506b1ed24d9d4394793e0479f170acc
stevewanash <stevewanash1@gmail.com>
2022-09-20 17:29:26 -0700

    password2

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,7 +1,3 @@
-#include <unistd.h>
-
-
-
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,36 +12,24 @@
 
 void randomPasswordGeneration(int N)
 {
-	// Initialize counter
 	int i = 0;
 
 	int randomizer = 0;
 
-	// Seed the random-number generator
-	// with current time so that the
-	// numbers will be different every time
 	srand((unsigned int)(time(NULL)));
 
-	// Array of numbers
 	char numbers[] = "0123456789";
 
-	// Array of small alphabets
 	char letter[] = "abcdefghijklmnoqprstuvwyzx";
 
-	// Array of capital alphabets
 	char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
 
-	// Array of all the special symbols
 	char symbols[] = "!@#$^&*?";
 
-	// Stores the random password
 	char password[N];
 
-	// To select the randomizer
-	// inside the loop
 	randomizer = rand() % 4;
 
-	// Iterate over the range [0, N]
 	for (i = 0; i < N; i++) 
 	{
 
