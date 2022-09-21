10d7c9108e1fde0a2b7a42671602aae3adb659f3
stevewanash <stevewanash1@gmail.com>
2022-09-21 13:21:11 -0700

    main

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,1 +1,7 @@
-char *string_toupper(char *);
+int _putchar(char c);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+char *string_toupper(char *a);
