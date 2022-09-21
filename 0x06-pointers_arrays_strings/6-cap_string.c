bba2d9ee3eefe340fd4350c39e032f31e855a04d
stevewanash <stevewanash1@gmail.com>
2022-09-21 14:20:02 -0700

    string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * cap_string - converts string to uppercase
+ * @a: string to be converted to uppercase
+ *
+ * Return: converted string
+ */
+char *cap_string(char *a);
+{
+
+	int c = 0;
+
+	while (a[c] != '\0')
+	{
+		if (a[c] >= 9 && a[c] <= 47 || a[c] >= 58 && a[c] <= 64 || a[c] >= 123 && a[c] <= 125)
+		{
+			if (a[c + 1] >= 97 && a[c + 1] <= 122)
+			{
+
+				a[c + 1] = 65 + (a[c + 1] % 97);
+
+				c++;
+
+				continue;
+
+			}
+
+			c++;
+
+			continue;
+
+		}
+
+		c++;
+
+	}
+
+	return (a);
+
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 char *string_toupper(char *a);
+char *cap_string(char *);
