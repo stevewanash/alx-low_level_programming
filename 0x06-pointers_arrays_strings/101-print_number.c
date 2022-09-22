e7603d3c6d4d4b6f4b2d907f75669590616ca079
stevewanash <stevewanash1@gmail.com>
2022-09-22 16:45:14 -0700

    number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_number - prints integers
+ * @value: integer to be printed
+ *
+ * no return
+ */
+void print_number(int n)
+{
+
+	if(value != 0)
+	{
+
+		print_number(value/10);
+
+		putchar((value%10) + '0');
+
+	}
+}
