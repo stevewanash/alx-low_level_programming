acd8ce24192346d8f07f722714b5ed5b8e4079c7
stevewanash <stevewanash1@gmail.com>
2022-09-27 16:53:21 -0700

    str

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * *_strchr - checks for a character in a string
+ * @s: The string where the character is checked
+ * @c: The character to be checked
+ *
+ * Return: On success, pointer to character in
+ * string
+ * 0 if character not found
+ * 
+ */
+char *_strchr(char *s, char c)
+{
+
+	int a = 0;
+
+	while (s[a] != c && s[a] != '\0')
+		a++;
+
+	return (s + a);
+
+}
