fc81e5ee08d748d14f62c549d0a359998cb0aad0
stevewanash <stevewanash1@gmail.com>
2022-09-29 16:14:46 -0700

    puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: The string to print
+ *
+ * Return: The printed string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+
+		_puts_recursion(s + 1);
+
+	}
+	else
+	{
+		_putchar('\n');
+
+		return;
+
+	}
+}
