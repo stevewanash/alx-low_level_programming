436bfe937d503ed4cdb6abc0c696176f181f5cee
stevewanash <stevewanash1@gmail.com>
2022-10-12 02:25:49 -0700

    structdog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+/**
+ * dog - gives attributes of dogs
+ * @name: gives the name of the dog
+ * @age: gives age of the dog
+ * @owner: gives the name of the dog owner
+ * 
+ */
+struct dog
+{
+    char *name;
+
+    float age;
+
+    char *owner;
+
+};
