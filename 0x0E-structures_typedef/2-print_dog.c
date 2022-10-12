0111499f1db9869f25d3ef86f2322367e00b5c54
stevewanash <stevewanash1@gmail.com>
2022-10-13 01:52:23 +0300

    printdog2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "dog.h"
 
+/**
+ * print_dog - prints details of the dog
+ * @d: variable of type struct dog
+ * 
+ * No return
+ */
 void print_dog(struct dog *d)
 {
     printf("Name: %s\nAge: %.06f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
