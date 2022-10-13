ed890e0004d9a83ba55212c88bf783c352572b1e
stevewanash <stevewanash1@gmail.com>
2022-10-13 04:26:48 +0300

    init2

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,9 +13,13 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    d->name = *name;
+    struct dog dog1;
 
-    d->age = *age;
+    d = &dog1;
 
-    d->owner = *owner;
+    *d->name = *name;
+
+    d->age = age;
+
+    *d->owner = *owner;
 }
