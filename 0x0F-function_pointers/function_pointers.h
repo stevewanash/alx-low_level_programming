978cc38957485ae6882aaa0d4316216aa2f5de26
stevewanash <stevewanash1@gmail.com>
2022-10-14 05:11:28 +0300

    array4

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,8 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
 typedef unsigned int size_t;
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+
+#endif
