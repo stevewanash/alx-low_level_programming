01e6b962d69857ed606aac029fbf75de650af4f3
stevewanash <stevewanash1@gmail.com>
2022-10-14 04:44:21 +0300

    array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * array_iterator - calls function for each element
+ * of an array
+ * @array: the array on which the function is called
+ * @size: size of the array
+ * @action: the function being called
+ * 
+ * No Return
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+    int i = 0;
+
+    while (i < size)
+    {
+        (*action)(array[i]);
+        
+        i++;
+    }
+}
