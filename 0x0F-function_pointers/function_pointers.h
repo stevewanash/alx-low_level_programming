04ab6bbc1422ce776448961e6f2ecf4649b39394
stevewanash <stevewanash1@gmail.com>
2022-10-14 05:58:20 +0300

    index3

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,3 @@
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-#ifndef TYPEDEF_SIZE_T_DECLARED_
-#define TYPEDEF_SIZE_T_DECLARED_
-typedef unsigned int size_t;
-#endif
