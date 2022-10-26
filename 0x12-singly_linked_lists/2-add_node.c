8a100d252d1e25e55674dded6cabcb2af0f98e06
stevewanash <stevewanash1@gmail.com>
2022-10-26 04:58:32 +0300

    addnode2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,6 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * *add_node - adds a node to the list
+ * @head: stores the address of the node being pointed
+ * to by head
+ * @str: is the string being copied to the str element
+ * 
+ * Return: address of new element on success
+ * NULL if it fails
+*/
 list_t *add_node(list_t **head, const char *str)
 {
     list_t *new;
@@ -16,4 +25,9 @@ list_t *add_node(list_t **head, const char *str)
     new -> len = strlen(str);
 
     *head = new;
+
+    if (*head != NULL)
+        return (*head);
+    else
+        return (NULL);
 }
